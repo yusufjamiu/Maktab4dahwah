bce8531033213d1c8621d10a37d776ce71a5d5a3
yusufjamiu <princeyusufjamiuadegbolahan@gmail.com>
2024-05-13 21:55:08 +0100

    Changes

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import image1 from '../assets/maktab4dahwah.jpg';
 import image2 from '../assets/distribution1.jpg';
 import image3 from '../assets/distribution2.jpg';
@@ -8,6 +8,7 @@ import image5 from '../assets/distribution3.jpg';
 const ImageSlider = () => {
   const images = [image1, image2, image3, image4, image5];
   const [currentSlide, setCurrentSlide] = useState(0);
+  const slideInterval = 3000; // Change this value to adjust the interval between slides
 
   const nextSlide = () => {
     setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
@@ -17,8 +18,13 @@ const ImageSlider = () => {
     setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
   };
 
+  useEffect(() => {
+    const slideTimer = setInterval(nextSlide, slideInterval);
+    return () => clearInterval(slideTimer);
+  }, [currentSlide, nextSlide, slideInterval]);
+
   return (
-    <div className="relative">
+    <div className="relative mt-32">
       <div className="flex overflow-hidden">
         {images.map((image, index) => (
           <img
@@ -46,6 +52,16 @@ const ImageSlider = () => {
         >
           &#8250;
         </button>
+      </div>
+      <div className="absolute bottom-4 left-0 right-0 flex justify-center space-x-2">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`h-2 w-2 rounded-full ${
+              index === currentSlide ? 'bg-gray-800' : 'bg-gray-400'
+            }`}
+          />
+        ))}
       </div>
     </div>
   );
